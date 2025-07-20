26236e56bbb64a13fe79e73f042f40dff049491b
Idespair <kawanptbr@gmail.com>
2025-07-20 17:46:36 -0300

    Client service layer updated

--- src/main/java/com/JutlandAir/JutlandAir/client/service/ClientService.java
+++ src/main/java/com/JutlandAir/JutlandAir/client/service/ClientService.java
@@ -1,5 +1,6 @@
 package com.JutlandAir.JutlandAir.client.service;
 
+import com.JutlandAir.JutlandAir.client.dto.ClientDTO;
 import com.JutlandAir.JutlandAir.client.model.Client;
 import com.JutlandAir.JutlandAir.client.repository.IClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,11 @@ public class ClientService {
 
     public List<Client> AllClients(){
         return clientRepository.findAll();
+    }
+
+    private ClientDTO clientToDTO(Client client){
+        return new ClientDTO(client.getId(), client.getName(), client.getLastName(),
+                client.getEmail(), client.getCountryOfBirth());
     }
 
 }
