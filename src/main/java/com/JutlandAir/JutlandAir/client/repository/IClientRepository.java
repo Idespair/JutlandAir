33592caffe6c77956a9d5e800b04dab58ad28f8d
Idespair <kawanptbr@gmail.com>
2025-07-20 01:17:23 -0300

    Client layers updated

--- src/main/java/com/JutlandAir/JutlandAir/client/repository/IClientRepository.java
+++ src/main/java/com/JutlandAir/JutlandAir/client/repository/IClientRepository.java
@@ -4,6 +4,8 @@ import com.JutlandAir.JutlandAir.client.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IClientRepository extends JpaRepository<Client, String> {
 
--- src/main/java/com/JutlandAir/JutlandAir/client/service/ClientService.java
+++ src/main/java/com/JutlandAir/JutlandAir/client/service/ClientService.java
@@ -1,13 +1,20 @@
 package com.JutlandAir.JutlandAir.client.service;
 
+import com.JutlandAir.JutlandAir.client.model.Client;
 import com.JutlandAir.JutlandAir.client.repository.IClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ClientService {
 
     @Autowired
     private IClientRepository clientRepository;
+
+    public List<Client> AllClients(){
+        return clientRepository.findAll();
+    }
 
 }
