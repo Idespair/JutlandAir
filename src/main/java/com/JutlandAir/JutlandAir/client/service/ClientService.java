5c92f28f72fbc848c72ff94d1b6916daa84414cb
Idespair <kawanptbr@gmail.com>
2025-07-19 21:36:01 -0300

    Service classes added

--- src/main/java/com/JutlandAir/JutlandAir/client/service/ClientService.java
+++ src/main/java/com/JutlandAir/JutlandAir/client/service/ClientService.java
@@ -1,7 +1,13 @@
 package com.JutlandAir.JutlandAir.client.service;
 
+import com.JutlandAir.JutlandAir.client.repository.IClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClientService {
+
+    @Autowired
+    private IClientRepository clientRepository;
+
 }
--- src/main/java/com/JutlandAir/JutlandAir/client/service/SeatService.java
+++ src/main/java/com/JutlandAir/JutlandAir/client/service/SeatService.java
@@ -1,7 +1,13 @@
 package com.JutlandAir.JutlandAir.client.service;
 
+import com.JutlandAir.JutlandAir.client.repository.ISeatRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SeatService {
+
+    @Autowired
+    private ISeatRepository seatRepository;
+
 }
