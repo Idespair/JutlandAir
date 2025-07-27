60e976852660bb17dea91b852edb6a1080cde62f
Idespair <kawanptbr@gmail.com>
2025-07-26 23:39:34 -0300

    Client controller updated.

--- src/main/java/com/JutlandAir/JutlandAir/client/controller/ClientController.java
+++ src/main/java/com/JutlandAir/JutlandAir/client/controller/ClientController.java
@@ -1,7 +1,13 @@
 package com.JutlandAir.JutlandAir.client.controller;
 
+import com.JutlandAir.JutlandAir.client.dto.request.ClientRequest;
 import com.JutlandAir.JutlandAir.client.service.ClientService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +18,9 @@ public class ClientController {
     @Autowired
     private ClientService clientService;
 
+    @PostMapping
+    public ResponseEntity<String> createClient(@Valid @RequestBody ClientRequest clientRequest){
+        clientService.createClient(clientRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body("User " + clientRequest.getName() + " created");
+    }
 }
