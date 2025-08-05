38ad9403ea6cf3923ab370f471cd134a5d25fd1b
Idespair <kawanptbr@gmail.com>
2025-08-05 20:45:45 -0300

    Controller updated.

--- src/main/java/com/JutlandAir/JutlandAir/client/controller/ClientController.java
+++ src/main/java/com/JutlandAir/JutlandAir/client/controller/ClientController.java
@@ -6,10 +6,9 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/client")
@@ -22,5 +21,12 @@ public class ClientController {
     public ResponseEntity<String> createClient(@Valid @RequestBody ClientRequest clientRequest){
         clientService.createClient(clientRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body("User " + clientRequest.getName() + " created");
+    }
+
+    @DeleteMapping("/{id}")
+    @PutMapping
+    public ResponseEntity<String> deleteClient(@PathVariable String id){
+        clientService.deleteUserByID(UUID.fromString(id));
+        return ResponseEntity.status(HttpStatus.valueOf(200)).body("User deleted successfully");
     }
 }
