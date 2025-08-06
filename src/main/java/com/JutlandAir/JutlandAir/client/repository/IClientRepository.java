45684e24840e492cbb0f346e458affa33287f5e0
Idespair <kawanptbr@gmail.com>
2025-08-05 23:29:40 -0300

    Method added.

--- src/main/java/com/JutlandAir/JutlandAir/client/repository/IClientRepository.java
+++ src/main/java/com/JutlandAir/JutlandAir/client/repository/IClientRepository.java
@@ -1,6 +1,8 @@
 package com.JutlandAir.JutlandAir.client.repository;
 
 import com.JutlandAir.JutlandAir.client.model.Client;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +11,8 @@ import java.util.UUID;
 @Repository
 public interface IClientRepository extends JpaRepository<Client, UUID> {
     void deleteById(UUID id);
+
+    Page<Client> findByNameContainingIgnoreCase (String name, Pageable page);
 
     boolean existsById(UUID id);
 }
